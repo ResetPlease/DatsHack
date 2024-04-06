20d544f375855cd82a83911d650bd995bd235012
Kirill Vekshin <vekshinkir@gmail.com>
2024-04-06 17:40:18 +0300

    ща можно закидывать путь в аргументы и смотреть на него

--- graph.py
+++ graph.py
@@ -6,7 +6,7 @@ matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 
 
-def show_graph():
+def show_graph(path = []):
     man = Interface(config.TOKEN)
     man.Register("universe", "https://datsedenspace.datsteam.dev/player/universe", "GET")
     info = man.universe()
@@ -28,5 +28,10 @@ def show_graph():
     edge_labels = nx.get_edge_attributes(G, 'weight')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6, font_weight='bold')
 
+    path_edges = [(path[i], path[i + 1]) for i in range(len(path) - 1)]
+    nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='r', arrows=True, arrowstyle='->', arrowsize=10)
+
     # Show the plot
     plt.show()
+
+#show_graph(['Earth', 'Orn', 'Bruen', 'ExuberantTiger', '7VY8G06B4', 'Kassulke-227', 'G2O32LU71', 'Kassulke-227', '7VY8G06B4', 'IKPMZKNH8', 'Gislason', 'LimeUnusual', 'Runte', 'Tremblay', '8Z8NGMGM7', 'Hettinger', 'BermudasCrawler', 'I8KOVD7V3', 'Bashirian-284', 'LemonBlack', 'Kautzer', 'TastyApro', 'Earth', 'BermudasCrawler', '9KXB300Y0', 'PanickedWatch', 'ApricotPoised9', 'SF6TPI3P4', 'CCPS4D1P5', 'Runte', 'TiredHerring81', 'LimeUnusual', 'Runte', 'Windler', 'Runte', 'LimeUnusual', 'Runte', 'Tremblay', '8Z8NGMGM7', 'Tremblay', 'Runte', 'TiredHerring81', 'LimeUnusual', 'Runte', 'Windler', 'TiredHerring81', 'LimeUnusual', 'Runte', 'LimeUnusual', 'Runte', 'TiredHerring81', 'Runte', 'LimeUnusual', 'Runte', 'Tremblay', 'Runte', 'TiredHerring81', 'Runte', 'TiredHerring81', 'Runte', 'LimeUnusual', 'Runte', 'Windler', 'Runte', 'TiredHerring81', 'Runte', 'TiredHerring81', 'Runte', 'LimeUnusual', 'Runte', 'TiredHerring81', 'LimeUnusual', 'Runte', 'Tremblay', 'Runte', 'TiredHerring81', 'Runte', 'LimeUnusual', 'Runte', 'Windler', 'Runte', 'Tremblay', 'Runte', 'LimeUnusual', 'Runte', 'TiredHerring81', 'Runte', 'TiredHerring81', 'Runte', 'LimeUnusual', 'Runte', 'Tremblay', '8Z8NGMGM7', 'Hettinger', 'LungWaiter6', 'EvilGoat', 'M4G20V5H7', 'J90ZI2R36', 'Hettinger', 'BermudasCrawler'])
